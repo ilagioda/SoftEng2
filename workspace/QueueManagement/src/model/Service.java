5c6c6e1b838509b1d88a968f0cbf895712d675e9
GianlucaBrezzo <gianlucabrezzo@gmail.com>
2019-10-16 17:23:45 +0200

    Implementata classe Service

--- workspace/QueueManagement/src/model/Service.java
+++ workspace/QueueManagement/src/model/Service.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Service {
-
+	
+	Service(String name, int waitTime){
+		this.name=name;
+		this.waitTime=waitTime;
+	}
+	
+	private String name;
+	private int waitTime;
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setWaitTime(int waitTime) {
+		this.waitTime = waitTime;
+	}
+	public int getWaitTime() {
+		return this.waitTime;
+	}
+	public String getName() {
+		return this.name;	
+	}
 }
+
