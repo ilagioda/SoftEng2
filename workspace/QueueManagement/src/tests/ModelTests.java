7495629bde9db4bf8c3978128d87cebc551d76e7
Claudio Filocamo <s256700@studenti.polito.it>
2019-10-17 21:02:33 +0200

    Fixed some warnings

--- workspace/QueueManagement/src/tests/ModelTests.java
+++ workspace/QueueManagement/src/tests/ModelTests.java
@@ -6,26 +6,18 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import model.Model;
-import model.Service;
-import model.Counter;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 public class ModelTests {
 
 	@Test
 	public void getNewTicketTest1() {
 		Model m = new Model();
-		Service ne = new Service("non_existing_name", "non_existing_code", 4);
 		assertEquals(m.getNewTicket("Non_existing_name"), null);
 	}
 	
 	@Test
 	public void getNewTicketTest2() {
-		Model m = new Model();
-		Service s1 = new Service("Shipping", "S", 10);
-		
+		Model m = new Model();		
 		assertTrue(m.getNewTicket("Shipping").contains("less than 1 minute."));
 		
 	}
@@ -34,7 +26,6 @@ public class ModelTests {
 	public void getNewTicketTest3() {
 		Model m = new Model();
 		int i;
-		Service s1 = new Service("Shipping", "S", 10);
 		
 		for(i=0; i<6; ++i)
 			m.getNewTicket("Shipping");
@@ -48,7 +39,6 @@ public class ModelTests {
 	public void getNewTicketTest4() {
 		Model m = new Model();
 		int i;
-		Service s1 = new Service("Shipping", "S", 10);
 		
 		for(i=0; i<13; ++i)
 			m.getNewTicket("Shipping");
@@ -67,16 +57,12 @@ public class ModelTests {
 	@Test
 	public void getNextTicketTest2() {
 		Model m = new Model();
-		Service s1 = new Service("Shipping", "S", 10);
-		Counter c = new Counter(0,new HashSet<Service>(Arrays.asList(s1)));
 		assertEquals(m.getNextTicket(0), ""); //no tickets to serve for that service
 	}
 	
 	@Test
 	public void getNextTicketTest3() {
 		Model m = new Model();
-		Service s1 = new Service("Shipping", "S", 10);
-		Counter c = new Counter(0,new HashSet<Service>(Arrays.asList(s1)));
 		m.getNewTicket("Shipping");
 		m.getNewTicket("Shipping");
 		assertTrue(m.getNextTicket(0).contains("Ticket S1"));
@@ -85,8 +71,6 @@ public class ModelTests {
 	@Test
 	public void getNextTicketTest4() {
 		Model m = new Model();
-		Service s1 = new Service("Shipping", "S", 10);
-		Counter c = new Counter(5,new HashSet<Service>());
 		m.getNewTicket("Shipping");
 		assertEquals(m.getNextTicket(5), null); //counter has no service associated
 	}
