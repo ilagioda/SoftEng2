8054097fe02b722a2aa316651051b7d3bba16646
Walter <walt3r1996@gmail.com>
2019-11-08 21:18:44 +0100

    database added

---
+++ ElectronicStudentRecordManagementSystem/createDB.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Creato il: Nov 08, 2019 alle 18:17
+-- Versione del server: 10.4.8-MariaDB
+-- Versione PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Admins`
+--
+
+CREATE TABLE `Admins` (
+  `codFisc` varchar(50) NOT NULL,
+  `hashedPassword` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Assignments`
+--
+
+CREATE TABLE `Assignments` (
+  `subject` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `classID` varchar(50) NOT NULL,
+  `textAssignment` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `lectures`
+--
+
+CREATE TABLE `lectures` (
+  `date` date NOT NULL,
+  `hour` int(11) NOT NULL,
+  `classID` int(11) NOT NULL,
+  `codFiscTeacher` int(11) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `topic` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Marks`
+--
+
+CREATE TABLE `Marks` (
+  `codFisc` varchar(50) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `hour` int(11) NOT NULL,
+  `mark` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Parents`
+--
+
+CREATE TABLE `Parents` (
+  `email` varchar(50) NOT NULL,
+  `hashedPassword` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `codFisc` varchar(50) NOT NULL,
+  `firstLogin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Principals`
+--
+
+CREATE TABLE `Principals` (
+  `codFisc` varchar(50) NOT NULL,
+  `hashedPassword` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Students`
+--
+
+CREATE TABLE `Students` (
+  `codFisc` varchar(50) NOT NULL,
+  `emailP1` varchar(50) NOT NULL,
+  `emailP2` varchar(50) NOT NULL,
+  `classID` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Teachers`
+--
+
+CREATE TABLE `Teachers` (
+  `codFisc` varchar(50) NOT NULL,
+  `hashedPassword` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `Admins`
+--
+ALTER TABLE `Admins`
+  ADD PRIMARY KEY (`codFisc`);
+
+--
+-- Indici per le tabelle `Marks`
+--
+ALTER TABLE `Marks`
+  ADD KEY `codFiscVincolo` (`codFisc`);
+
+--
+-- Indici per le tabelle `Parents`
+--
+ALTER TABLE `Parents`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indici per le tabelle `Principals`
+--
+ALTER TABLE `Principals`
+  ADD PRIMARY KEY (`codFisc`);
+
+--
+-- Indici per le tabelle `Students`
+--
+ALTER TABLE `Students`
+  ADD PRIMARY KEY (`codFisc`);
+
+--
+-- Indici per le tabelle `Teachers`
+--
+ALTER TABLE `Teachers`
+  ADD PRIMARY KEY (`codFisc`);
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `Marks`
+--
+ALTER TABLE `Marks`
+  ADD CONSTRAINT `codFiscVincolo` FOREIGN KEY (`codFisc`) REFERENCES `Students` (`codFisc`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
