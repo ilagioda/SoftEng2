636d6d41c29b0d541e6cfad96747ae0792b664ee
Walter <walt3r1996@gmail.com>
2019-11-09 12:13:29 +0100

    Normal Page of classComposition done.
    Page to request a particular composition
    In the dbAdmin class the method readAllClasses() is created.

--- ElectronicStudentRecordManagementSystem/code/classComposition.php
+++ ElectronicStudentRecordManagementSystem/code/classComposition.php
@@ -1,99 +1,213 @@
 <?php
-require_once 'basicChecks.php';
+require_once('basicChecks.php');
+require_once('db.php');
+$dbAdmin = new dbAdmin();
 
-echo <<<_MAIN
-    <body>
-    <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>                        
-        </button>
-        <a class="navbar-brand" href="index.php"><img class="logo" src=images/logo.png></a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </ul>
-        </div>
-    </div>
-    </nav>
-    
-    <div class="container-fluid text-center">    
-    <div class="row content">
-        <div class="col-sm-2 sidenav">
-        <p><a href="#">Link</a></p>
-        <p><a href="#">Link</a></p>
-        <p><a href="#">Link</a></p>
-        </div>
-        <div class="col-sm-8 text-left"> 
-        <h1>Welcome</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <hr>
-        <div class="container">
-  <h2>Acceptance of class composition</h2>
-  <p>The class composition is:</p>            
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Class</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>1A</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>1A</td>
-      </tr>
-      <tr>
-    </tbody>
-  </table>
-</div>
+if (isset($_POST["view"])) {
+  //if set it means that the user requested the composition of a class
 
-<div class="form-group">        
-<div class="col-sm-offset-2 col-sm-10">
-  <button type="submit" class="btn btn-default">Submit</button>
-</div>
-</div>
+  //echo sizeof($_POST);
+
+  foreach ($_POST as $key => $value) {
+    if ($key != "view") {
+      //
+      echo $value;
 
 
+      echo <<<REQUESTEDPAGE
+        <body>
+          <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><img class="logo" src=images/logo.png> </a> </div> <div class="collapse navbar-collapse" id="myNavbar">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                  </ul>
+              </div>
+            </div>
+          </nav>
 
-        <h3>Test</h3>
-        <p>Lorem ipsum...</p>
-        </div>
-        <div class="col-sm-2 sidenav">
-        <div class="well">
-            <p>ADS</p>
-        </div>
-        <div class="well">
-            <p>ADS</p>
-        </div>
-        </div>
-    </div>
-    </div>
+          <div class="container-fluid text-center">
+            <div class="row content">
+              <div class="col-sm-2 sidenav">
+                <p><a href="#">Link</a></p>
+                <p><a href="#">Link</a></p>
+                <p><a href="#">Link</a></p>
+              </div>
+              <div class="col-sm-8 text-left">
+                <h1>Welcome</h1>
+                <p>The purpose of this page is to select the class in order to see and accept the class composition.</p>
+                <hr>
+                <div class="container">
+                  <h2>Classes</h2>
+                  <table class="table table-hover">
+                    <tbody>
+REQUESTEDPAGE;
+
+      foreach ($classes as $value) {
+        echo <<< ROW
+                            <tr>
+                            <td>$value</td>
+                            <td>
+                            <form method="post" class="form-horizontal" action="./classComposition.php">
+                              <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                <input type="text" name="$value"  hidden value="$value">
+                                  <button type="submit" name="view" class="btn btn-default">View</button>
+                            </form>
+                            </td>
+                            </tr>
+ROW;
+      }
+      echo <<<ENDOFREQUESTEDPAGE
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
 
 
-    <footer class="container-fluid text-center">
-    <p>Footer Text</p>
-    </footer>
 
-    </body>
-    </html>
-_MAIN;
 
+            <footer class="container-fluid text-center">
+              <p>Footer Text</p>
+            </footer>
+
+        </body>
+
+        </html>
+ENDOFREQUESTEDPAGE;
+    }
+  }
+} else {
+//print the normal page
+  
+  $classes = $dbAdmin->readAllClasses();
+  
+  echo <<<NORMALPAGE
+        <body>
+          <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><img class="logo" src=images/logo.png> </a> </div> <div class="collapse navbar-collapse" id="myNavbar">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                  </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div class="container-fluid text-center">
+            <div class="row content">
+              <div class="col-sm-2 sidenav">
+                <p><a href="#">Link</a></p>
+                <p><a href="#">Link</a></p>
+                <p><a href="#">Link</a></p>
+              </div>
+              <div class="col-sm-8 text-left">
+                <h1>Welcome</h1>
+                <p>The purpose of this page is to select the class in order to see and accept the class composition.</p>
+                <hr>
+                <div class="container">
+                  <h2>Classes</h2>
+                  <table class="table table-hover">
+                    <tbody>
+NORMALPAGE;
+
+  foreach ($classes as $value) {
+    echo <<< ROW
+                            <tr>
+                            <td>$value</td>
+                            <td>
+                            <form method="post" class="form-horizontal" action="./classComposition.php">
+                              <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                <input type="text" name="$value"  hidden value="$value">
+                                  <button type="submit" name="view" class="btn btn-default">View</button>
+                            </form>
+                            </td>
+                            </tr>
+ROW;
+  }
+  echo <<<ENDOFNORMALPAGE
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <footer class="container-fluid text-center">
+              <p>Footer Text</p>
+            </footer>
+
+        </body>
+
+        </html>
+ENDOFNORMALPAGE;
+}
 ?>
+
+
+
+<!-- <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Class</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>1A</td>
+                <td>
+                  <form method="post" class="form-horizontal" action="./classComposition.php">
+                    <div class="form-group">
+                      <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default" name>View</button>
+                  </form>
+                </td>
+              </tr>
+              <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>1A</td>
+                <td>
+                  <form class="form-horizontal" action="./classComposition.php">
+                    <div class="form-group">
+                      <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default">View</button>
+                  </form>
+                </td>
+              </tr>
+              <tr>
+            </tbody> */
+            -->
--- ElectronicStudentRecordManagementSystem/code/db.php
+++ ElectronicStudentRecordManagementSystem/code/db.php
@@ -2,33 +2,57 @@
 
 require_once("basicChecks.php");
 
-class db{
+class db
+{
 
     function __construct()
     {
         $servername = "localhost";
-        $username = "username";
-        $password = "password";
+        $username = "root";
+        $password = "";
         $dbname = "school";
 
         // Create connection
-        $this->conn = new mysqli($servername, $username, $password,$dbname);
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
 
         // Check connection
-        if ($this->conn->connect_error){
+        if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
             $this->conn->close();
         }
     }
 }
 
-class dbAdmin extends db{
+class dbAdmin extends db
+{
 
+    function readClassCompositions($class)
+    { }
+
+    function readAllClasses()
+    {
+
+        $sql = "SELECT classID FROM ProposedClasses";
+        $resultQuery = $this->conn->query($sql);
+
+        if ($resultQuery->num_rows > 0) {
+            $resultArray = array();
+            // output data of each row
+            while ($row = $resultQuery->fetch_assoc()) {
+                array_push($resultArray,$row["classID"]);
+            }
+            return $resultArray;
+        }
+    }
 }
 
-class dbParent extends db{
 
-    public function viewChildMarks($CodFisc){
+
+class dbParent extends db
+{
+
+    public function viewChildMarks($CodFisc)
+    {
         /* 
         This function receives the fiscal code of a child, 
         verifies if the child is actually a child of that parent
@@ -39,9 +63,3 @@ class dbParent extends db{
         return "";
     }
 }
-
-class dbTeacher extends db{
-
-}
-
-?>
