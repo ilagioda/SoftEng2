c583e503a377c72a89e53bb7f7419c9cb378e35b
Claudio Filocamo <s256700@studenti.polito.it>
2019-11-13 14:36:05 +0100

    mailInterface and styles.css changed

--- ElectronicStudentRecordManagementSystem/code/mailInterface.php
+++ ElectronicStudentRecordManagementSystem/code/mailInterface.php
@@ -10,15 +10,20 @@ $_SESSION['db'] = new dbAdmin();
 <script>
 
 function sendMail(id, cl){
+  element = document.getElementById("td_load" + cl);
+  element_before = document.getElementById("td_load" + cl).innerHTML;
+  element.innerHTML='<div class="loader"></div>';
     email="";
     $.post("sendmail.php", {'mail' : id},
     function(response){
       if(response.includes("Message has been sent.")){
-        document.getElementById("answer").innerHTML = '<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong><span class="glyphicon glyphicon-send"></span> Success! Message sent.</strong></div>';
-        document.getElementById(cl).className="danger";
+        document.getElementById("answer").innerHTML = document.getElementById("answer").innerHTML + '<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong><span class="glyphicon glyphicon-send"></span> Success! Credentials sent to '+ id +'.</strong></div>';
+        document.getElementById(cl).className="";
+        document.getElementById("td_load"+cl).innerHTML = element_before;
       }
       else{
-        document.getElementById("answer").innerHTML = '<div class="alert alert-danger alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong><span class="glyphicon glyphicon-send"></span> Error. Message has not been sent.</strong></div>';
+        document.getElementById("answer").innerHTML = '<div class="alert alert-danger alert-dismissble"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong><span class="glyphicon glyphicon-send"></span> Error. Message has not been sent.</strong></div>';
+        document.getElementById("td_load"+cl).innerHTML = element_before;
       }
     });
 }
@@ -27,8 +32,8 @@ function sendMail(id, cl){
 
 <div class="container">
   <h2>Emails to be confirmed</h2>
-  <span class="label label-success col-md-3 col-md-push-9">Email not sent yet</span><br>
-  <span class="label label-danger col-md-3 col-md-push-9">Email sent, but still to be confirmed</span>
+  <span class="badge badge-pill badge-yellow col-md-3 col-md-push-9">Email not sent yet</span><br>
+  <span class="badge badge-pill badge-white col-md-3 col-md-push-9">Email sent, but still to be confirmed</span>
   <p class="text-center">Search by email address:</p>
   <input class="form-control" id="myInput" type="text" placeholder="Search..">
   <br>
@@ -39,7 +44,7 @@ function sendMail(id, cl){
 
 
   </div>
-  <table class="table table-striped sendmail">
+  <table class="table sendmail">
     <thead>
       <tr>
         <th>Email</th>
@@ -57,17 +62,17 @@ foreach ($ParentsMailList as $tuple) {
     if ($hashedPw == null && $firstLogin == true) {
         //email ancora da inviare
         echo <<<_SUCCESS
-            <tr class="success" id="r$i">
+            <tr class="warning" id="r$i">
             <td>$email</td>
-            <td class="td_resized_sendmail"><button type="button" class="btn btn-success primary btn-lg" id="$email" onclick="sendMail(id, 'r$i')">SEND</td>
+            <td class="td_resized_sendmail" id="td_loadr$i"><button type="button" class="btn btn-success primary btn-lg" id="$email" onclick="sendMail(id, 'r$i')">SEND</td>
           </tr>
         _SUCCESS;
     } else if ($hashedPw != null && $firstLogin == true) {
         //email già mandata, ma utente non ha ancora modificato pw (c'è quella di default)
         echo <<<_SENT
-            <tr class="danger" id="r$i">
+            <tr class="default" id="r$i">
             <td>$email</td>
-            <td class="td_resized_sendmail"><button type="button" class="btn btn-success primary btn-lg" id="$email" onclick="sendMail(id, 'r$i')">SEND</td>
+            <td class="td_resized_sendmail" id="td_loadr$i"><button type="button" class="btn btn-success primary btn-lg" id="$email" onclick="sendMail(id, 'r$i')">SEND</td>
             </tr>
           _SENT;
     }
@@ -89,8 +94,16 @@ $(document).ready(function(){
     $("#myTable tr").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
+
+    
   });
+
+  $(".alert").delay(4000).slideUp(200, function() {
+    $(this).alert('close');
 });
+
+});
+
 </script>
 
 <!-- //USEFUL TO PRINT ALERT MESSAGES IF FORM IS OK OR NOT
