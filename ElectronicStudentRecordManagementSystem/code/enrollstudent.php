7ecd5c4a21f937c95169b44953318f95d4000b46
Ilaria <s257312@studenti.polito.it>
2019-11-14 21:36:02 +0100

    Adapt the "enroll student" table to the window

--- ElectronicStudentRecordManagementSystem/code/enrollstudent.php
+++ ElectronicStudentRecordManagementSystem/code/enrollstudent.php
@@ -54,21 +54,21 @@
 	<form method="POST" action="enrollstudentPART2.php" id="enrollForm">
 	<table class="enrollTable">
 		<tr class="info"><td><b>STUDENT INFORMATION</b></td><td></td></tr>
-		<tr><td><label for="name">Name*: </label></td><td><input type="text" size="50" name="name" id="name" placeholder="Enter the student's name" autocomplete="off" title="Enter the student's name" required></td></tr>
-		<tr><td><label for="surname">Surname*: </label></td><td><input type="text" size="50" name="surname" id="surname" placeholder="Enter the student's surname" autocomplete="off" title="Enter the student's surname" required></td></tr>
-		<tr><td><label for="codfisc">SSN code*: </label></td><td><input type="text" size="50" name="codfisc" id="codfisc" placeholder="Enter the student's SSN code" autocomplete="off" title="Enter the student's SSN code" required></td></tr>
-		<tr><td></td></tr>
+		<tr><td class="col-md-6" ><label for="name">Name*: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="name" id="name" placeholder="Enter the student's name" autocomplete="off" title="Enter the student's name" required></td></tr>
+		<tr><td class="col-md-6"><label for="surname">Surname*: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="surname" id="surname" placeholder="Enter the student's surname" autocomplete="off" title="Enter the student's surname" required></td></tr>
+		<tr><td class="col-md-6"><label for="codfisc">SSN code*: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="codfisc" id="codfisc" placeholder="Enter the student's SSN code" autocomplete="off" title="Enter the student's SSN code" required></td></tr>
+		<tr><td class="col-md-6"></td></tr>
 		<tr class="info"><td><b>PARENT 1 INFORMATION</b></td><td></td></tr>
-		<tr><td><label for="name1">Name*: </label></td><td><input type="text" size="50" name="name1" id="name1" placeholder="Enter the parent 1's name" autocomplete="off" title="Enter the student's name" required></td></tr>
-		<tr><td><label for="surname1">Surname*: </label></td><td><input type="text" size="50" name="surname1" id="surname1" placeholder="Enter the parent 1's surname" autocomplete="off" title="Enter the student's surname" required></td></tr>
-		<tr><td><label for="codfisc1">SSN code*: </label></td><td><input type="text" size="50" name="codfisc1" id="codfisc1" placeholder="Enter the parent 1's SSN code" autocomplete="off" title="Enter the student's SSN code" required></td></tr>
-		<tr><td><label for="email1">E-mail*: </label></td><td><input type="email" size="50" name="email1" id="email1" placeholder="Enter the parent 1's e-mail" autocomplete="off" title="Enter the parent 2's e-mail" required></td></tr>
-		<tr><td></td></tr>
-		<tr class="info" id="rowParent2" onclick="changeVisibility()"><td><span id="iconParent2" class="glyphicon glyphicon-plus" aria-hidden="true">&emsp;&emsp;</span><b>PARENT 2 INFORMATION</b></td><td></td></tr>
-		<tr id="row1" style="visibility: hidden;"><td><label for="name2">Name: </label></td><td><input type="text" size="50" name="name2" id="name2" placeholder="Enter the parent 2's name" autocomplete="off" title="Enter the student's name"></td></tr>
-		<tr id="row2" style="visibility: hidden;"><td><label for="surname2">Surname: </label></td><td><input type="text" size="50" name="surname2" id="surname2" placeholder="Enter the parent 2's surname" autocomplete="off" title="Enter the student's surname"></td></tr>
-		<tr id="row3" style="visibility: hidden;"><td><label for="codfisc2">SSN code: </label></td><td><input type="text" size="50" name="codfisc2" id="codfisc2" placeholder="Enter the parent 2's SSN code" autocomplete="off" title="Enter the student's SSN code"></td></tr>
-		<tr id="row4" style="visibility: hidden;"><td><label for="email2">E-mail: </label></td><td><input type="email" size="50" name="email2" id="email2" placeholder="Enter the parent 2's e-mail" autocomplete="off" title="Enter the parent 2's e-mail"></td></tr>
+		<tr><td class="col-md-6"><label for="name1">Name*: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="name1" id="name1" placeholder="Enter the parent 1's name" autocomplete="off" title="Enter the student's name" required></td></tr>
+		<tr><td class="col-md-6"><label for="surname1">Surname*: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="surname1" id="surname1" placeholder="Enter the parent 1's surname" autocomplete="off" title="Enter the student's surname" required></td></tr>
+		<tr><td class="col-md-6"><label for="codfisc1">SSN code*: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="codfisc1" id="codfisc1" placeholder="Enter the parent 1's SSN code" autocomplete="off" title="Enter the student's SSN code" required></td></tr>
+		<tr><td class="col-md-6"><label for="email1">E-mail*: </label></td><td class="col-md-6"><input type="email" style="width:100%" name="email1" id="email1" placeholder="Enter the parent 1's e-mail" autocomplete="off" title="Enter the parent 2's e-mail" required></td></tr>
+		<tr><td class="col-md-6"></td></tr>
+		<tr class="info" id="rowParent2" onclick="changeVisibility()"><td class="col-md-6"><span id="iconParent2" class="glyphicon glyphicon-plus" aria-hidden="true">&emsp;&emsp;</span><b>PARENT 2 INFORMATION</b></td><td></td></tr>
+		<tr id="row1" style="visibility: hidden;"><td class="col-md-6"><label for="name2">Name: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="name2" id="name2" placeholder="Enter the parent 2's name" autocomplete="off" title="Enter the student's name"></td></tr>
+		<tr id="row2" style="visibility: hidden;"><td class="col-md-6"><label for="surname2">Surname: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="surname2" id="surname2" placeholder="Enter the parent 2's surname" autocomplete="off" title="Enter the student's surname"></td></tr>
+		<tr id="row3" style="visibility: hidden;"><td class="col-md-6"><label for="codfisc2">SSN code: </label></td><td class="col-md-6"><input type="text" style="width:100%" name="codfisc2" id="codfisc2" placeholder="Enter the parent 2's SSN code" autocomplete="off" title="Enter the student's SSN code"></td></tr>
+		<tr id="row4" style="visibility: hidden;"><td class="col-md-6"><label for="email2">E-mail: </label></td><td class="col-md-6"><input type="email" style="width:100%" name="email2" id="email2" placeholder="Enter the parent 2's e-mail" autocomplete="off" title="Enter the parent 2's e-mail"></td></tr>
 	</table>
 		<input type="reset" name="enrollCancel" id="enrollCancel" value="Cancel"> 
 		<input type="submit" class="pulsante" name="enrollSubmit" id="enrollSubmit" value="Enroll student">
