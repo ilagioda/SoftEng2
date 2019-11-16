162462708dca2b3d3ff5391eb6925a5536539f4d
simo-gen <56380499+simo-gen@users.noreply.github.com>
2019-11-16 13:34:48 +0100

    add for selecting subjects dynamically

---
+++ ElectronicStudentRecordManagementSystem/code/selectSubjects.php
@@ -0,0 +1,22 @@
+<?php	
+	$_SESSION['user']="GNV";
+	$_SESSION['role']="teacher";
+	
+	require_once("classTeacher.php");    
+	$teacher=new Teacher();
+
+	$subjects = array();
+
+	if(isset($_POST["comboClass"])) {
+		
+		$selectedClass = $_POST["comboClass"];
+		$subjects = $teacher->getSubjectByClassAndTeacher($selectedClass);
+		
+		$output = "";
+		
+		foreach($subjects as $subject) {
+			$output .= "<option value=".$subject.">".$subject."</option>";
+		}
+		echo $output;
+	}
+?>
