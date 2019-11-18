c13a63030e5b532efc3f2f618fd57207cbbc99e2
Marco <ciprianomarco@outlook.it>
2019-11-18 15:17:20 +0100

    Downloaded PHPUNIT 8 and made it available to everyone

---
+++ ElectronicStudentRecordManagementSystem/code/tests/DBTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class dbTest extends TestCase{
+
+    public function testGetSubjectTaughtInClass(): void{
+        $this->assertSame(0,0);
+    }
+}
+?>
