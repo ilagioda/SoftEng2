3eae3e531fef8a87400c59e25d20ddc82cab0764
Ilaria <s257312@studenti.polito.it>
2019-11-18 22:53:33 +0100

    Centered homepage buttons

--- ElectronicStudentRecordManagementSystem/code/index.php
+++ ElectronicStudentRecordManagementSystem/code/index.php
@@ -15,17 +15,17 @@ $_SESSION = array();
             <p><a href="#">Link</a></p>
             <p><a href="#">Link</a></p>
         </div>
-        <div class="col-sm-8 text-left">
+        <div class="col-sm-8 text-center">
             <h1>Welcome</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <hr>
             <h3> Who are you? </h3>
             <div class="btn-group-vertical" role="group">
                 
-                <a href="homepagePrincipal.php" class="btn btn-large btn-block btn-info" role="button">Principal</a>
-                <a href="homepageTeacher.php" class="btn btn-large btn-block btn-info" role="button">Teacher</a>
-                <a href="pseudoLogParent.php" class="btn btn-large btn-block btn-info" role="button">Parent</a>
-                <a href="homepageAdmin.php" class="btn btn-large btn-block btn-info" role="button">Admin</a>
+                <a href="homepagePrincipal.php" class="btn btn-large btn-block btn-primary" role="button">Principal</a>
+                <a href="homepageTeacher.php" class="btn btn-large btn-block btn-primary" role="button">Teacher</a>
+                <a href="pseudoLogParent.php" class="btn btn-large btn-block btn-primary" role="button">Parent</a>
+                <a href="homepageAdmin.php" class="btn btn-large btn-block btn-primary" role="button">Admin</a>
             </div>
         </div>
         <div class="col-sm-2 sidenav">
