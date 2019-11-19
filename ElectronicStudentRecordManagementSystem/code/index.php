df1a9bcb3b4c6e6b9e007b5f07baf006c1b81cec
Claudio Filocamo <s256700@studenti.polito.it>
2019-11-19 18:59:48 +0100

    Logo added

--- ElectronicStudentRecordManagementSystem/code/index.php
+++ ElectronicStudentRecordManagementSystem/code/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once("basicChecks.php");
+require_once "basicChecks.php";
 
 $loggedin = false;
 if (isset($_SESSION['user']) && $_SESSION['role'] == "teacher") {
@@ -28,27 +28,19 @@ if (!$loggedin) {
         <div class="col-sm-8 text-center">
             <h1>Welcome</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <hr>
-            <h3> Who are you? </h3>
-            <div class="btn-group-vertical" role="group">
-                
-                <a href="homepagePrincipal.php" class="btn btn-large btn-block btn-primary" role="button">Principal</a>
-                <a href="homepageTeacher.php" class="btn btn-large btn-block btn-primary" role="button">Teacher</a>
-                <a href="pseudoLogParent.php" class="btn btn-large btn-block btn-primary" role="button">Parent</a>
-                <a href="homepageAdmin.php" class="btn btn-large btn-block btn-primary" role="button">Admin</a>
-            </div>
+            <img id="central_image" src="images/school_logo.png">
         </div>
         <div class="col-sm-2 sidenav">
             <div class="well">
                 <p>ADS</p> <!--Logo facebook -->
             </div>
             <div class="well">
-                <p>ADS</p> <!--Logo istagram -->
+                <p>ADS</p> <!--Logo instagram -->
             </div>
         </div>
     </div>
 </div>
 
 <?php
-require_once("defaultFooter.php")
+require_once "defaultFooter.php"
 ?>
