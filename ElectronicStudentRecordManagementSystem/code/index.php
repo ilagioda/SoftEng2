048e9fdb73f907881270f19a8694a88ca1221608
Claudio Filocamo <s256700@studenti.polito.it>
2019-11-19 10:42:25 +0100

    refactored index.php

--- ElectronicStudentRecordManagementSystem/code/index.php
+++ ElectronicStudentRecordManagementSystem/code/index.php
@@ -1,17 +1,24 @@
 <?php
 require_once("basicChecks.php");
 require_once("defaultNavbar.php");
-
 /**
  *  To be removed => here until logout is implemented
  * */
 $_SESSION = array();
 ?>
 
+<div class="container-fluid text-center">
+    <div class="row content">
+        <div class="col-sm-2 sidenav">
+            <p><a href="#">Link</a></p>
+            <p><a href="#">Link</a></p>
+            <p><a href="#">Link</a></p>
+        </div>
         <div class="col-sm-8 text-left">
             <h1>Welcome</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <hr>
-            <h2> Who are you? </h2>
+            <h3> Who are you? </h3>
             <div class="btn-group-vertical" role="group">
                 
                 <a href="homepagePrincipal.php" class="btn btn-large btn-block btn-primary" role="button">Principal</a>
@@ -20,7 +27,16 @@ $_SESSION = array();
                 <a href="homepageAdmin.php" class="btn btn-large btn-block btn-primary" role="button">Admin</a>
             </div>
         </div>
- 
+        <div class="col-sm-2 sidenav">
+            <div class="well">
+                <p>ADS</p>
+            </div>
+            <div class="well">
+                <p>ADS</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 require_once("defaultFooter.php")
