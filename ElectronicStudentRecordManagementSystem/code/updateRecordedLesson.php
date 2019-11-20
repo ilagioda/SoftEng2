d6cf1c3f530516104ca2504207e76d93e5579f67
GianlucaBrezzo <gianlucabrezzo@gmail.com>
2019-11-20 13:57:14 +0100

    Corrected another role for login

--- ElectronicStudentRecordManagementSystem/code/updateRecordedLesson.php
+++ ElectronicStudentRecordManagementSystem/code/updateRecordedLesson.php
@@ -2,7 +2,7 @@
 	require_once("basicChecks.php");
 	
 	$loggedin = false;
-if (isset($_SESSION['user']) && $_SESSION['role'] == "admin") {
+if (isset($_SESSION['user']) && $_SESSION['role'] == "teacher") {
     $loggedin = true;
 }
 if (!$loggedin) {
