9992b8bb1a597cd9cbe7ee04701e6d2d97f6a7c3
Ilaria <s257312@studenti.polito.it>
2019-11-26 22:22:51 +0100

    Deleted useless css of enrollStudent and fixed buttons in homepageAdmin

--- ElectronicStudentRecordManagementSystem/code/homepageAdmin.php
+++ ElectronicStudentRecordManagementSystem/code/homepageAdmin.php
@@ -19,18 +19,17 @@ $_SESSION['role'] = 'admin'; */
 
 /* End lines to be changed*/
 ?>
-<h1 align="center"> ADMIN HOMEPAGE </h1>
-<div class="text-center">
-	<p>
-        <?php
-echo "<h2>Welcome to your homepage ADMIN " . $_SESSION["user"] . "!</h2><br>";
-?>
 
-	</p>
-    <a href="enrollstudent.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-user pull-left" aria-hidden="true"></span>&emsp;Enroll new student</a><br><br>
-    <a href="mailInterface.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-envelope pull-left" aria-hidden="true"></span>&emsp;Enable access parents</a><br><br>
-    <a href="classComposition.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-list-alt pull-left" aria-hidden="true"></span>&emsp;Class composition</a><br><br>
-    <a href="setupAccounts.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-book pull-left" aria-hidden="true"></span>&emsp;Setup official accounts</a><br><br>
+<div class="text-center">
+    <h1> ADMIN HOMEPAGE </h1>
+    <?php
+        echo "<h2>Welcome to your homepage ADMIN " . $_SESSION["user"] . "!</h2><br>";
+    ?>
+
+    <a href="enrollstudent.php" class="btn btn-primary btn-lg main" role="button"><span class="glyphicon glyphicon-user pull-left" aria-hidden="true"></span>&emsp;Enroll new student</a><br>
+    <a href="mailInterface.php" class="btn btn-primary btn-lg main" role="button"><span class="glyphicon glyphicon-envelope pull-left" aria-hidden="true"></span>&emsp;Enable access parents</a><br>
+    <a href="classComposition.php" class="btn btn-primary btn-lg main" role="button"><span class="glyphicon glyphicon-list-alt pull-left" aria-hidden="true"></span>&emsp;Class composition</a><br>
+    <a href="setupAccounts.php" class="btn btn-primary btn-lg main" role="button"><span class="glyphicon glyphicon-book pull-left" aria-hidden="true"></span>&emsp;Setup official accounts</a><br>
 </div>
 
 <?php
