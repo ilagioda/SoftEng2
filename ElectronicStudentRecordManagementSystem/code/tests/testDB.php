a4eb05b17df744417bc85f8214c8ee31b2716013
GianlucaBrezzo <53696327+GianlucaBrezzo@users.noreply.github.com>
2019-12-07 20:35:05 +0100

    Added first tests (still WIP)

--- ElectronicStudentRecordManagementSystem/code/tests/testDB.php
+++ ElectronicStudentRecordManagementSystem/code/tests/testDB.php
@@ -1,10 +1,11 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_TestCase;
 
 require_once("../classTeacher.php");
+require_once("../db.php");
 
-final class dbTest extends TestCase{
+final class dbTest extends PHPUnit_Framework_TestCase{
 
     public function testGetSubjectTaughtInClass(): void{
         $this->assertSame(1,1);
@@ -35,6 +36,53 @@ final class dbTest extends TestCase{
 
         //Se è stata settata un uscita e l'ora di uscita < ora entrata
         $this->assertEquals($attendance['absence'] == 1);
+    }
+
+
+
+
+    ///////////////////////////////////////////////////////////
+
+    public function testViewStudentMarks(): void {
+
+        $_SESSION['role'] = "teacher";
+        $_SESSION['user'] = "test";
+        $db = new dbTeacher();
+
+
+        //Studente non presente nel db o senza voti
+        $CodFisc = "absent";
+        $subject = "Italian";
+
+        $result = $db->viewStudentMarks($CodFisc, $subject);
+
+        $this->assertSame($result, null);
+
+
+        //Studente presente e con voti (si assume che si testi dopo aver caricato il db)
+        $CodFisc = "FRCWTR";
+
+        $result = $db->ViewStudentMarks($CodFisc, $subject);
+
+        $marks=array();
+        array_push($marks,"2019-10-15,9,3");
+        array_push($marks,"2019-10-14,9/10,2");
+        array_push($marks,"2019-10-10,7+,1");
+
+        $this->assertSame($result, $marks);
+
+
+        //Errore nella query
+        $result = $db->viewStudentMarks($subject, $CodFisc);
+        $this->assertSame($result,null);
+
+    }
+
+    public function testrecordEarlyExit(){
+        $_SESSION['role'] = "teacher";
+        $_SESSION['user'] = "test";
+        $db = new dbTeacher();
+
     }
 
 
