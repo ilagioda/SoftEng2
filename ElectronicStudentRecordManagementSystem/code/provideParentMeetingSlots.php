381899d47e6c1dda330a36499360972974109a90
Ilaria <s257312@studenti.polito.it>
2019-12-11 15:15:33 +0100

    Continued implementation of story#21

--- ElectronicStudentRecordManagementSystem/code/provideParentMeetingSlots.php
+++ ElectronicStudentRecordManagementSystem/code/provideParentMeetingSlots.php
@@ -177,7 +177,9 @@ require_once("db.php");
             'day': selecteddate,
             'codFisc': fiscalCode
         }), function(text) {
-            
+
+            // TODO ----------------------------------------------------------------------------------------------------------------
+
             // Show the list of time slots 8:00-9:00, 9:00-10:00, 10:00-11:00, 11:00-12:00, 12:00-13:00, 13:00-14:00
             var slots = document.getElementById("daySlots");
             var str = "";
